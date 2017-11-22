275a6bc161c1e85ac41700c6d349460c01c54f05
Vojtěch Mádr <madrvojtech@outlook.com>
2017-11-22 10:17:18 +0100

    Fix script for macos

--- Sunshine/appcenter-pre-build.sh
+++ Sunshine/appcenter-pre-build.sh
@@ -8,11 +8,10 @@ GITCOUNT=$(git rev-list --all --count)
 NEWVERSIONNAME="$ENVIRONMENT_VARIABLE.$GITCOUNT"
 
 if [ "$APPCENTER_BRANCH" == "master" ];
-
 then
 echo "Package name is $PACKAGENAME and new is $NEWNAME";
 echo "Old version name is $VERSIONNAME and new version name is $NEWVERSIONNAME"
 sed -i '' 's/package=*"'$PACKAGENAME'"/package="'$NEWNAME'"/g' $MANIFEST_FILE
-sed -i 's/versionName="'$VERSIONNAME'"/versionName="'$NEWVERSIONNAME'"/g' $MANIFEST_FILE
+sed -i '' 's/versionName="'$VERSIONNAME'"/versionName="'$NEWVERSIONNAME'"/g' $MANIFEST_FILE
 fi
 
