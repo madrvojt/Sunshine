bb8ac9017f95ed022c3fa23f42ac52417a7a927d
Vojtěch Mádr <madrvojtech@outlook.com>
2017-11-22 10:07:09 +0100

    Update script

--- Sunshine/appcenter-pre-build.sh
+++ Sunshine/appcenter-pre-build.sh
@@ -2,12 +2,17 @@
 
 MANIFEST_FILE='Properties/AndroidManifest.xml'
 PACKAGENAME=`grep "package" $MANIFEST_FILE | sed 's/.*package="//;s/".*//'`
+VERSIONNAME=`grep "versionName" $MANIFEST_FILE | sed 's/.*versionName="//;s/".*//'`
 NEWNAME=$PACKAGENAME.DEBUG
+GITCOUNT=$(git rev-list --all --count)
+NEWVERSIONNAME="$ENVIRONMENT_VARIABLE.$GITCOUNT"
 
 if [ "$APPCENTER_BRANCH" == "master" ];
+
 then
-
 echo "Package name is $PACKAGENAME and new is $NEWNAME";
+echo "Old version name is $VERSIONNAME and new version name is $NEWVERSIONNAME"
 sed -i '' 's/package=*"'$PACKAGENAME'"/package="'$NEWNAME'"/g' $MANIFEST_FILE
-
+sed -i 's/versionName="'$VERSIONNAME'"/versionName="'$NEWVERSIONNAME'"/g' $MANIFEST_FILE
 fi
+
