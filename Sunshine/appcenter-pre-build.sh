b823d790ddfe159b5add8c8a0f0313906300d381
Vojtech Madr <madrvojtech@outlook.com>
2018-10-07 23:06:57 +0200

    Update Build Script

--- Sunshine/appcenter-pre-build.sh
+++ Sunshine/appcenter-pre-build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 MANIFEST_FILE='Properties/AndroidManifest.xml'
-PACKAGENAME=`grep "package" $MANIFEST_FILE | sed 's/.*package="//;s/".*//'`
-VERSIONNAME=`grep "versionName" $MANIFEST_FILE | sed 's/.*versionName="//;s/".*//'`
-NEWNAME=$PACKAGENAME.DEBUG
-GITCOUNT=$(git rev-list --all --count)
-NEWVERSIONNAME="$APP_VERSION.$GITCOUNT"
+#PACKAGENAME=`grep "package" $MANIFEST_FILE | sed 's/.*package="//;s/".*//'`
+#VERSIONNAME=`grep "versionName" $MANIFEST_FILE | sed 's/.*versionName="//;s/".*//'`
+#NEWNAME=$PACKAGENAME.DEBUG
+#GITCOUNT=$(git rev-list --all --count)
+#NEWVERSIONNAME="$APP_VERSION.$GITCOUNT"
 
-if [ "$APPCENTER_BRANCH" == "master" ];
-then
-echo "Package name is $PACKAGENAME and new is $NEWNAME";
-echo "Old version name is $VERSIONNAME and new version name is $NEWVERSIONNAME"
-sed -i '' 's/package=*"'$PACKAGENAME'"/package="'$NEWNAME'"/g' $MANIFEST_FILE
-sed -i '' 's/versionName="'$VERSIONNAME'"/versionName="'$NEWVERSIONNAME'"/g' $MANIFEST_FILE
-fi
+#if [ "$APPCENTER_BRANCH" == "master" ];
+#echo "Package name is $PACKAGENAME and new is $NEWNAME";
+#then
+#echo "Old version name is $VERSIONNAME and new version name is $NEWVERSIONNAME"
+#sed -i '' 's/package=*"'$PACKAGENAME'"/package="'$NEWNAME'"/g' $MANIFEST_FILE
+#sed -i '' 's/versionName="'$VERSIONNAME'"/versionName="'$NEWVERSIONNAME'"/g' $MANIFEST_FILE
+#fi
 
